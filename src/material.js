2dcccbf26fc411328e70f6dd508fa7d294b8f390
Mel <melchrstmn@gmail.com>
2020-06-23 22:31:56 -0400

    added array, push all new instances into array

--- src/material.js
+++ src/material.js
@@ -1,10 +1,14 @@
 class Material {
-//i don't think data is a real thing here. category_id is all that is accessible
+//all of these things show properly in debugger but still says Uncaught TypeError: Cannot read property 'title' of undefined when i type new Material(material)
     constructor(material, materialAttributes) {
         this.id = material.id
+        //debugger
         this.title = materialAttributes.title
         this.description = materialAttributes.description
         this.url = materialAttributes.url
         this.category = materialAttributes.category
+        Material.all.push(this)
     }
 }
+
+Material.all = [];
