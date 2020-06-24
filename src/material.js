eb6f4032c38ad883b2140f981cb8e334427c42f1
Mel <melchrstmn@gmail.com>
2020-06-23 21:50:13 -0400

    built out constructor

--- src/material.js
+++ src/material.js
@@ -2,5 +2,9 @@ class Material {
 //i don't think data is a real thing here. category_id is all that is accessible
     constructor(data) {
         this.id = data.id
+        this.title = data.title
+        this.description = data.description
+        this.url = data.url
+        this.category = data.category
     }
 }
